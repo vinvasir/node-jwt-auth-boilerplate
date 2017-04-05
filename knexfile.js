33e89068910d836a0fedf4cffd9edec534ce09d6
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-04-05 13:09:54 -0400

    add production configuratoion options for the db connection

--- knexfile.js
+++ knexfile.js
@@ -35,5 +35,16 @@ module.exports = {
       tableName: 'knex_migrations'
     }
   },
-
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+    ssl: true
+  },
 };
