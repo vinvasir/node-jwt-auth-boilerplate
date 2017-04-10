c149577be13c4016edae920ae9e09e1b033eba36
Prateek Vasireddy <prat.vasireddy@gmail.com>
2017-04-10 12:28:55 -0400

    start using the faker library. recursively call tests from the entire test directory. set up a test with more seed data

--- test/app.test.js
+++ test/app.test.js
@@ -1,10 +1,9 @@
-const expect = require('chai').expect;
-const request = require('supertest');
+global.expect = require('chai').expect;
+global.request = require('supertest');
+global.faker = require('faker');
 
 const dbConfig = require('../knexfile.js');
 const knex = require('knex')(dbConfig.test);
-
-const Post = require('../models/post.js');
 
 beforeEach(done => {
 	knex.migrate.rollback()
@@ -14,18 +13,4 @@ beforeEach(done => {
 				done();
 			})
 		}).catch(err => done(err))
-	// done();
-});
-
-describe('Post #create', () => {
-	it('should create a post', done => {
-		let post = {title: 'first post', body: 'nothing much'}
-
-		Post.forge(post).save()
-			.then(post => {
-				expect(post.attributes.title).to.equal('first post');
-				done();
-			})
-			.catch(e => done(e));
-	});
 });
---
+++ test/models/post.test.js
@@ -0,0 +1,47 @@
+// const dbConfig = require('../../knexfile.js');
+// const knex = require('knex')(dbConfig.test);
+
+const Post = require('../../models/post.js');
+
+describe('Post #create', () => {
+	it('should create a post', done => {
+		let post = {title: 'first post', body: 'nothing much'}
+
+		Post.forge(post).save()
+			.then(post => {
+				expect(post.attributes.title).to.equal('first post');
+
+				Post.fetchAll().then(posts => {
+					expect(posts.length).to.equal(1);
+					done();
+				})
+			})
+			.catch(e => done(e));
+	});
+});
+
+describe('Post #all', () => {
+	beforeEach(done => {
+		var promises = [];
+
+		for(let i=0, l=5; i < l; i++) {
+			promises.push(
+				Post.forge({
+					title: faker.lorem.word(),
+					body: faker.lorem.paragraph()
+				}).save()
+			);
+		}
+		 Promise.all(promises).then(values => {
+			 done();
+		}).catch(err => done(err));
+	})
+
+	it('should return all posts', done => {
+		Post.fetchAll()
+			.then(posts => {
+				expect(posts.length).to.equal(5);
+				done();
+			})
+	})
+})
